7662b58f8328f5a894b8dfe765651c9c0ef69cb4
Igor Ryzhakov <maggie991@bk.ru>
2023-05-24 16:12:28 +0300

    Advanced_Project

---
+++ config-client/src/main/java/comm/SpringConfigClientApplication.java
@@ -0,0 +1,14 @@
+package comm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfigClientApplication.class, args);
+    }
+}
