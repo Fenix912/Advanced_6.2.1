1c4b30fc63f352496aa889e712c2d8742d19427c
Igor Ryzhakov <maggie991@bk.ru>
2023-05-25 15:35:41 +0300

    Advanced_Project

---
+++ book-service/src/main/java/com/example/bookservice/service/BookService.java
@@ -0,0 +1,9 @@
+package com.example.bookservice.service;
+
+import com.example.bookservice.entity.Book;
+
+import java.util.List;
+
+public interface BookService {
+    List<Book> findAllBooks();
+}
--- config-client/src/test/java/configclient/example/configclient/ConfigClientApplicationTests.java
+++ config-client/src/test/java/configclient/example/configclient/ConfigClientApplicationTests.java
@@ -1,4 +1,4 @@
-package comm.example.configclient;
+package configclient.example.configclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
